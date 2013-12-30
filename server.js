21cfeef353f55c777677b1fc10f10060d3e34924
savelichalex <savelichalex93@gmail.com>
2013-12-31 00:22:45 +0700

    change PORT in server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var http = require("http"),
-	url = require("url");
+	url = require("url"),
+	PORT = +process.env.PORT || 1337;
+
 function start(route, handle) {;
 http.createServer(function(req, res){
 	var pathname = url.parse(req.url).pathname;
 	console.log("Request for" + pathname);
 	route(pathname, handle, res);
-}).listen(3000);
+}).listen(PORT);
 }
 
 exports.start = start;
